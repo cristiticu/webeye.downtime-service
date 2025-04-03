fa7414313939d13321ce0b57f3d0c5ac5fce3c83
Cristian Ticu <cristian.ticu@eggnita.com>
2025-04-03 17:28:42 +0300

    Separate checker from service

--- src/routers/downtimes.py
+++ src/routers/downtimes.py
@@ -22,21 +22,3 @@ def list_events(check_type: str, url: str, start_date: str, end_date: str, token
     events = application_context.downtimes.get_events(
         token.user_guid, url, check_type, start_date, end_date)
     return events
-
-
-@router.get("/status/{check_type}")
-def get_current_status(check_type: str, url: str, token: Annotated[UserTokenData, Depends(user_token_data)]):
-    status = application_context.downtimes.get_current_status_or_create(
-        token.user_guid, url, check_type)
-    return status
-
-
-@router.post("/http")
-def run_http_check(url: str, token: Annotated[UserTokenData, Depends(user_token_data)]):
-    response = application_context.downtimes.run_http_uptime(
-        token.user_guid, url)
-    return response
-
-
-# @router.get("events/{check_type}/{url:path}")
-# def list_events()
